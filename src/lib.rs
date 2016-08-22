2ed4ddc8b2ebc15a011ceefcfffb85cc26ef5100
Judson <nyarly@gmail.com>
2016-08-22 16:28:20 -0700

    Ch1 works

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,14 @@ pub mod set1 {
       return BigInt::parse_bytes(hex.as_bytes(), 16).ok_or("Couldn't parse as hex")
     }
 
-    fn bigint2base64<'a>(num: BigInt) -> Result<&'a str, &'static str> {
+    fn bigint2base64(num: BigInt) -> Result<String, &'static str> {
       let (_, bytes) = num.to_bytes_be();
 
-      return Ok(&bytes.to_base64(STANDARD))
+      return Ok(bytes.to_base64(STANDARD))
     }
 
-    pub fn hex2base64(hex: &str) -> Result<&str, &'static str> {
-      bigint2base64(hex2bigint(hex))
+    pub fn hex2base64(hex: &str) -> Result<String, &'static str> {
+      hex2bigint(hex).and_then(|num| bigint2base64(num))
     }
     #[cfg(test)]
     mod tests {
@@ -34,7 +34,7 @@ pub mod set1 {
       #[test]
       fn it_works() {
         assert_eq!(
-          super::hex2base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"),
+          super::hex2base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").unwrap(),
           "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
           )
       }
