243a5cdfff33c315dca46ab62c57a2d791d6dc79
Judson <nyarly@gmail.com>
2016-09-04 15:51:36 -0700

    Converting to a RMSD implementation on scoring

--- src/set1/mod.rs
+++ src/set1/mod.rs
@@ -11,6 +11,7 @@ pub mod utils {
   use serialize::base64::{STANDARD,ToBase64};
   use std::ops::BitXor;
   use std::iter::{self,FromIterator,IntoIterator};
+  use std::collections::btree_map::BTreeMap;
 
   pub fn hex2bigint(hex: &str) -> Result<BigInt,&'static str> {
     return BigInt::parse_bytes(hex.as_bytes(), 16).ok_or("Couldn't parse as hex")
@@ -43,9 +44,9 @@ pub mod utils {
         None => None,
         Some(v) =>
           Some(list.fold(v, |best, candidate| {
-            let (max_score, _) = best;
+            let (min_score, _) = best;
             let (score, _) = candidate;
-            if score > max_score {
+            if score < min_score {
               candidate
             } else {
               best
@@ -68,7 +69,85 @@ pub mod utils {
     xor_iters(crypted, iter::repeat(&key))
   }
 
+  pub fn frequency_counts(bytes: &[u8]) -> BTreeMap<u8,u32> {
+    let mut fc = BTreeMap::new();
+    let mut count = 0;
+    for c in bytes {
+      let entry = fc.entry(*c).or_insert(0);
+      *entry += 1;
+      count += 1
+    }
+    let factor = 1000.0 / count as f64;
+    for (_, count) in fc.iter_mut() {
+      *count = (*count as f64 * factor) as u32
+    }
+    fc
+  }
+
+  fn congruent_counts(like: BTreeMap<u8,u32>, from: BTreeMap<u8,u32>) -> BTreeMap<u8, u32> {
+    let mut res = BTreeMap::new();
+    for key in like.keys() {
+      res.insert(*key, from.get(key).map(|kr| kr.clone()).unwrap_or(0));
+    }
+    res
+  }
+
+  fn english_freqs() -> BTreeMap<u8,u32> {
+    let mut ef = BTreeMap::new();
+    ef.insert(b' ', 130);
+    ef.insert(b'e', 127);
+    ef.insert(b'E', 127);
+    ef.insert(b't', 90);
+    ef.insert(b'T', 90);
+    ef.insert(b'a', 81);
+    ef.insert(b'A', 81);
+    ef.insert(b'o', 75);
+    ef.insert(b'O', 75);
+    ef.insert(b'\n', 75);
+    ef.insert(b'\t', 75);
+    ef.insert(b'\r', 75);
+    ef.insert(b'i', 69);
+    ef.insert(b'I', 69);
+    ef.insert(b'n', 67);
+    ef.insert(b'N', 67);
+    ef.insert(b's', 63);
+    ef.insert(b'S', 63);
+    ef.insert(b'h', 60);
+    ef.insert(b'H', 60);
+    ef.insert(b'r', 59);
+    ef.insert(b'R', 59);
+    ef.insert(b'd', 42);
+    ef.insert(b'D', 42);
+    ef.insert(b'l', 40);
+    ef.insert(b'L', 40);
+    ef.insert(b'c', 27);
+    ef.insert(b'C', 27);
+    ef.insert(b'u', 27);
+    ef.insert(b'U', 27);
+    ef
+  }
+
+  fn squares_of_differences<'g, I: IntoIterator<Item=&'g u32>>(left: I, right: I) -> Vec<u32> {
+    left.into_iter().zip(right.into_iter())
+      .map(|(&l,&r)| (l as i32 - r as i32).pow(2) as u32)
+      .collect()
+  }
+
+  fn rmsd<'g, I>(left: I, right: I) -> u32
+    where I: IntoIterator<Item=&'g u32>
+  {
+    let sod = squares_of_differences(left, right);
+    (sod.iter().fold(0, |acc, n| acc + n) as f64 / sod.len() as f64).sqrt() as u32
+  }
+
+  // lower is better now
   pub fn english_score(bytes: &[u8]) -> u32 {
+    let fc = frequency_counts(bytes);
+
+    rmsd(congruent_counts(english_freqs(), fc).values(), english_freqs().values())
+  }
+
+  pub fn old_english_score(bytes: &[u8]) -> u32 {
     bytes.iter()
       .fold(1, |score, letter|
             if score == 0 {
@@ -128,9 +207,11 @@ pub mod utils {
 
     #[test]
     fn scores_english() {
-      assert!(string_score("some words") > 0);
-      assert!(string_score("some words") > string_score("zxcvb"));
-      assert!(string_score(";;;;;") == 1)
+      assert!(string_score("some words") > 0,
+              "score = {}", string_score("some_words"));
+      assert!(string_score("some words") < string_score("zxcvb"),
+              "'some words' = {} 'zxcvb' = {}", string_score("some words"), string_score("zxcvb"));
+      assert!(string_score(";;;;;") > 71, "';;;;;' = {}", string_score(";;;;;"))
     }
 
   }
