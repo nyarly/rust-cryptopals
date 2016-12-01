6c4653abc5065a1ecf690d522a77c62c6f8c245d
Judson <nyarly@gmail.com>
2016-12-01 08:06:40 -0800

    Starting ch8

--- src/byte_convert.rs
+++ src/byte_convert.rs
@@ -2,28 +2,41 @@ use num_bigint::BigInt;
 use serialize::base64::{self, ToBase64};
 use result::{Result, CrackError};
 use std::fs::File;
-use std::io::{BufReader, Read};
+use std::io::{BufReader, BufRead, Read};
 use ::serialize::base64::FromBase64;
 
 pub fn hex2bigint(hex: &str) -> ::std::result::Result<BigInt, &'static str> {
-    return BigInt::parse_bytes(hex.as_bytes(), 16).ok_or("Couldn't parse as hex");
+  return BigInt::parse_bytes(hex.as_bytes(), 16).ok_or("Couldn't parse as hex");
 }
 
 pub fn bigint2base64(num: BigInt) -> ::std::result::Result<String, &'static str> {
-    let (_, bytes) = num.to_bytes_be();
+  let (_, bytes) = num.to_bytes_be();
 
-    return Ok(bytes.to_base64(base64::STANDARD));
+  return Ok(bytes.to_base64(base64::STANDARD));
 }
 
 pub fn hex2bytes(hex: &str) -> ::std::result::Result<Vec<u8>, &'static str> {
-    let (_, uint) = try!(hex2bigint(hex)).to_bytes_be();
-    Ok(uint)
+  let (_, uint) = try!(hex2bigint(hex)).to_bytes_be();
+  Ok(uint)
+}
+
+pub fn open_hexlines_path(path: &str) -> Result<Vec<Vec<u8>>> {
+  let file = try!(File::open(path));
+  let mut buf = BufReader::new(file);
+  let mut lines = Vec::new();
+  try!(buf.read_to_end(&mut lines));
+  (&lines)
+    .lines()
+    .map(|line| {
+         let string = try!(line);
+         hex2bytes(&string).map_err(|s| CrackError::from(s))
+    }).collect()
 }
 
 pub fn open_base64_path(path: &str) -> Result<Vec<u8>> {
-    let file = try!(File::open(path));
-    let mut buf = BufReader::new(file);
-    let mut b64bytes = Vec::new();
-    try!(buf.read_to_end(&mut b64bytes));
-    b64bytes.from_base64().map_err(|e| CrackError::from(e))
+  let file = try!(File::open(path));
+  let mut buf = BufReader::new(file);
+  let mut b64bytes = Vec::new();
+  try!(buf.read_to_end(&mut b64bytes));
+  b64bytes.from_base64().map_err(|e| CrackError::from(e))
 }
--- src/set1/challenge8.rs
+++ src/set1/challenge8.rs
@@ -1,4 +1,6 @@
-
+use super::super::byte_convert;
+use super::super::result::Result;
+use super::frequency;
 
 ///Detect AES in ECB mode
 /// In this file are a bunch of hex-encoded ciphertexts.
@@ -12,5 +14,22 @@
 /// same 16 byte ciphertext.
 ///
 /// Examples
-pub fn detect_aes_ecb(path &str) -> Result<Option<String>> {
+///
+/// ```
+/// assert_eq!(
+///   cryptopal::set1::challenge8::detect_aes_ecb("./s1c8.txt").unwrap().unwrap(),
+///   "zxcv"
+///   )
+/// ```
+pub fn detect_aes_ecb(path: &str) -> Result<Option<Vec<u8>>> {
+    let crypts = try!(byte_convert::open_hexlines_path(path));
+
+    for crypt in crypts {
+        let chunks = (&crypt).chunks(16);
+        let c = frequency::Counts::new(chunks);
+        if c.sorted_counts()[0] > 1 {
+            return Ok(Some(crypt));
+        }
+    }
+    return Ok(None);
 }
