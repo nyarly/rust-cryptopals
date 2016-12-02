342a9ca53acd309551428ac197f3ba4ca7b7f85b
Judson <nyarly@gmail.com>
2016-12-01 16:55:10 -0800

    Poking at types

--- src/set1/challenge8.rs
+++ src/set1/challenge8.rs
@@ -2,7 +2,7 @@ use super::super::byte_convert;
 use super::super::result::Result;
 use super::frequency;
 
-///Detect AES in ECB mode
+/// Detect AES in ECB mode
 /// In this file are a bunch of hex-encoded ciphertexts.
 ///
 /// One of them has been encrypted with ECB.
@@ -25,7 +25,7 @@ pub fn detect_aes_ecb(path: &str) -> Result<Option<Vec<u8>>> {
     let crypts = try!(byte_convert::open_hexlines_path(path));
 
     for crypt in crypts {
-        let chunks = (&crypt).chunks(16);
+        let chunks = (&crypt).chunks(16).map(|ch| &String::from_utf8_lossy(ch)).collect::<Vec<_>>();
         let c = frequency::Counts::new(chunks);
         if c.sorted_counts()[0] > 1 {
             return Ok(Some(crypt));
